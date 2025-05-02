5a18eb5e6b7893b7a22b4fa77604e279f0265ae5
Utiashvili1 <tatoutiashviliut@gmail.com>
2025-05-02 16:35:02 +0400

    second commit

---
+++ day 02/classwork/python.py
@@ -0,0 +1,8 @@
+
+name = "Tato" # String
+
+age = 17 # Integer
+
+num = 12.7 # float
+
+is_student = True # Boolean
---
+++ day 02/homework/python.py
@@ -0,0 +1,39 @@
+name = input("Enter your Name: ")
+
+print(name)
+
+print(30+13)
+print(23+43)
+print(19+17)
+print(44+24)
+print(67+25)
+
+print(58-23)
+print(32-19)
+print(65-23)
+print(85-67)
+print(76-47)
+
+print(23*6)
+print(45*9)
+print(124*23)
+print(285*13)
+print(584*11)
+
+print(350/5)
+print(500/2)
+print(1560/6)
+print(100/5)
+print(220/11)
+
+name2 = "Tato"
+username = "utiashvili"
+age = 17
+color = "white"
+car = "BMW M5 F90"
+
+print(name2)
+print(username)
+print(age)
+print(color)
+print(car)
