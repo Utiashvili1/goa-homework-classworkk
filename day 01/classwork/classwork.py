cdecc16b9c319cabf3344132dea122dfa50f7c8a
Utiashvili1 <tatoutiashviliut@gmail.com>
2025-05-02 15:34:23 +0400

    second commit

--- day 01/classwork/classwork.py
+++ day 01/classwork/classwork.py
@@ -1,1 +1,2 @@
-print("no classwork")
+print("Hello word")
+
--- day 01/homework/homework.py
+++ day 01/homework/homework.py
@@ -1,1 +1,16 @@
-print("no homework")
+name = "tato"
+color = "white"
+age = 17
+friends_age = 18
+friends_age = 19
+friends_age = 20
+friends_age = 21
+family_members = "mari"
+family_members = "ramazi"
+family_members = "taisia"
+animals = True
+
+##string is a type of information where we have only alphabetical text, no numbers or decimals
+#integer is an type of information where we have whole numbers, no alphabetical text ordecimas
+#float is an type of information where we have decimals, no alphabetical text or whole numbers
+#boolean has no decimals, alphabetical text or whole numbers, it have only 2 forms true or false
