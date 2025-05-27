8f55d32836c2e6b320fa43c4566da262157e1e78
Utiashvili1 <tatoutiashviliut@gmail.com>
2025-05-27 17:53:33 +0400

    second commit

---
+++ day 09/classwork/python.py
@@ -0,0 +1,13 @@
+int1=int(input("start:"))
+int2=int(input("step:"))
+int3=int(input("end:"))
+
+for i in range(int1,int2,int3):
+    print(i)
+
+int1=int(input("num:"))
+
+i=0
+while i !=int1:
+    print(i)
+    i += 1
